9115534346eef888076d0a676d7e35305d59f801
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-10-10 13:52:40 +0200

    feat: validation
    Add validation regexp

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Message = ({ firstName, lastName }) => (
-  <div>
-    firstName: {firstName}
-    lastName: {lastName}
-  </div>
-);
+const Message = ({ firstName, firstNameError, lastName, lastNameError }) => {
+  return (
+    <div>
+      <span className={firstNameError ? "green" : "red"}>firstName:</span>{" "}
+      {firstName}
+      <span className={lastNameError ? "green" : "red"}>lastName:</span>{" "}
+      {lastName}
+    </div>
+  );
+};
 
 export default Message;
--- src/index.js
+++ src/index.js
@@ -7,25 +7,46 @@ import "./styles.css";
 
 class App extends Component {
   state = {
-    firstName: "",
-    lastName: ""
+    firstName: { content: "", error: false },
+    lastName: { content: "", error: false },
+    autre: { content: "", error: false }
+  };
+
+  validInput = e => {
+    let InputTarget = e.target.name;
+    let obj = this.state[InputTarget];
+    const regexp = /[A-Za-z]{3,}/;
+    this.setState({
+      [InputTarget]: { ...obj, error: regexp.test(e.target.value) }
+    });
+    console.log(obj);
   };
 
   changeInput = e => {
     let InputTarget = e.target.name;
-    console.log(InputTarget, this.state);
-    this.setState({ [InputTarget]: e.target.value });
+    let obj = this.state[InputTarget];
+    this.setState({ [InputTarget]: { ...obj, content: e.target.value } });
+    //console.log(this.state.firstName);
+  };
+
+  composeChange = e => {
+    //this.changeInput(e);
+    this.validInput(e);
   };
 
   render() {
+    const { firstName } = this.state;
+    const { lastName } = this.state;
     return (
       <div className="App">
         <h1>Data Binding</h1>
-        <Inputbox name="firstName" onChange={this.changeInput} />
-        <Inputbox name="lastName" onChange={this.changeInput} />
+        <Inputbox name="firstName" onChange={this.composeChange} />
+        <Inputbox name="lastName" onChange={this.composeChange} />
         <Message
-          firstName={this.state.firstName}
-          lastName={this.state.lastName}
+          firstName={firstName.content}
+          lastName={lastName.content}
+          firstNameError={firstName.error}
+          lastNameError={lastName.error}
         />
       </div>
     );
